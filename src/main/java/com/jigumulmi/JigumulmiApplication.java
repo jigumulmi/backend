95e1afbd07456abf15acdbdc64e7e8e5e8720f27
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-03-24 02:57:49 +0900

    initial setting

---
+++ src/main/java/com/jigumulmi/JigumulmiApplication.java
@@ -0,0 +1,14 @@
+package com.jigumulmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigumulmiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JigumulmiApplication.class, args);
+    }
+
+}
