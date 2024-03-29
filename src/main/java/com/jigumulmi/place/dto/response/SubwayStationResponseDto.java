b50caa8474bd8cefb901e71f9aae3fb6aab395a2
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-03-30 02:39:57 +0900

    Feat: 지하철역 엔티티 확장성 대비 컬럼 추가

--- src/main/java/com/jigumulmi/place/dto/response/SubwayStationResponseDto.java
+++ src/main/java/com/jigumulmi/place/dto/response/SubwayStationResponseDto.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Builder
 public class SubwayStationResponseDto {
     private Long id;
-    private String name;
-    private String line;
+    private String stationName;
+    private String lineNumber;
 }
