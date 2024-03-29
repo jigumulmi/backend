bc84f1a841d15e428e2c039f4be77cb3dbfbb642
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-03-30 00:05:47 +0900

    Docs: swagger 도입 (미완)

---
+++ src/main/java/com/jigumulmi/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.jigumulmi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Jigumulmi API")
+                        .description("지구멀미")
+                        .version("0.1.0"));
+    }
+}
