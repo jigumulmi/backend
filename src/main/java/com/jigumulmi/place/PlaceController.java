982833f2171093b681af4c48a5a9fb5ea6844585
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-03-30 20:23:58 +0900

    Docs: swagger docs

--- src/main/java/com/jigumulmi/place/PlaceController.java
+++ src/main/java/com/jigumulmi/place/PlaceController.java
@@ -3,6 +3,12 @@ package com.jigumulmi.place;
 import com.jigumulmi.place.dto.request.CreatePlaceRequestDto;
 import com.jigumulmi.place.dto.response.RestaurantResponseDto;
 import com.jigumulmi.place.dto.response.SubwayStationResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,18 +24,28 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
+    @Operation(summary = "지하철역 조회")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = SubwayStationResponseDto.class)))})}
+    )
     @GetMapping("/subway")
     public ResponseEntity<?> getSubwayStations(@RequestParam(name = "stationName") String stationName) {
         List<SubwayStationResponseDto> subwayStationList = placeService.getSubwayStationList(stationName);
         return ResponseEntity.ok().body(subwayStationList);
     }
 
+    @Operation(summary = "장소 등록 신청")
+    @ApiResponse(responseCode = "201")
     @PostMapping("")
     public ResponseEntity<?> registerPlace(@Valid @RequestBody CreatePlaceRequestDto requestDto) {
         placeService.registerPlace(requestDto);
         return new ResponseEntity<>("Register success", HttpStatus.CREATED);
     }
 
+    @Operation(summary = "장소 리스트 조회")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = RestaurantResponseDto.class)))})}
+    )
     @GetMapping("")
     public ResponseEntity<?> getPlaceList(@RequestParam(name = "subwayStationId", required = false) Long subwayStationId) {
         List<RestaurantResponseDto> placeList = placeService.getPlaceList(subwayStationId);
