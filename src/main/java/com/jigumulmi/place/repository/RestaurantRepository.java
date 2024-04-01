764b0a77fecf5faf988398b8fc5bd4a00fbb41b4
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-04-01 18:45:27 +0900

    Refactor: 장소 리스트 조회 쿼리 개선

--- src/main/java/com/jigumulmi/place/repository/RestaurantRepository.java
+++ src/main/java/com/jigumulmi/place/repository/RestaurantRepository.java
@@ -1,17 +1,17 @@
 package com.jigumulmi.place.repository;
 
 import com.jigumulmi.place.domain.Restaurant;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-    @Query("select r from Restaurant r left join fetch r.menuList m where r.isApproved = true")
+    @EntityGraph(attributePaths = {"menuList"})
     List<Restaurant> findAllByIsApprovedTrue();
 
-    @Query("select r from Restaurant r left join fetch r.menuList m where r.subwayStation.id = :subwayStationId and r.isApproved = true")
+    @EntityGraph(attributePaths = {"menuList"})
     List<Restaurant> findAllBySubwayStationIdAndIsApprovedTrue(@Param("subwayStationId") Long subwayStationId);
 
 }
