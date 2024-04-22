8d99a731b025a7ffc5d2def622e401a0d25cb13e
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-04-22 23:52:56 +0900

    BugFix: 스프링 기본 타임존 설정

--- src/main/java/com/jigumulmi/JigumulmiApplication.java
+++ src/main/java/com/jigumulmi/JigumulmiApplication.java
@@ -1,12 +1,19 @@
 package com.jigumulmi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class JigumulmiApplication {
+    @PostConstruct
+    void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JigumulmiApplication.class, args);
