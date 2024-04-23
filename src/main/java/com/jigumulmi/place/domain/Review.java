60990db79b6bc1003081bac4e91498c7bbbe60f4
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-04-24 00:22:52 +0900

    Feat: 리뷰 기능 엔티티 모델링

---
+++ src/main/java/com/jigumulmi/place/domain/Review.java
@@ -0,0 +1,47 @@
+package com.jigumulmi.place.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.jigumulmi.config.common.Timestamped;
+import com.jigumulmi.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Review extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer rating;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    @JsonBackReference
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "review")
+    @JsonManagedReference
+    private List<ReviewReply> reviewReplyList = new ArrayList<>();
+
+    @Builder
+    public Review(Integer rating, String content, Member member, Restaurant restaurant, List<ReviewReply> reviewReplyList) {
+        this.rating = rating;
+        this.content = content;
+        this.member = member;
+        this.restaurant = restaurant;
+        this.reviewReplyList = reviewReplyList;
+    }
+}
---
+++ src/main/java/com/jigumulmi/place/domain/ReviewReply.java
@@ -0,0 +1,36 @@
+package com.jigumulmi.place.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.jigumulmi.config.common.Timestamped;
+import com.jigumulmi.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class ReviewReply extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    @JsonBackReference
+    private Review review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public ReviewReply(String content, Review review, Member member) {
+        this.content = content;
+        this.review = review;
+        this.member = member;
+    }
+}
