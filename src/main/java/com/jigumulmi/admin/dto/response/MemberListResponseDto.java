60cf7a6c619efce40de064102a41f6beca30244d
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-05-15 23:04:04 +0900

    Feat: 조회 응답 형식 서비스 api와 통일

--- src/main/java/com/jigumulmi/admin/dto/response/MemberListResponseDto.java
+++ src/main/java/com/jigumulmi/admin/dto/response/MemberListResponseDto.java
@@ -1,30 +1,31 @@
 package com.jigumulmi.admin.dto.response;
 
 import com.jigumulmi.member.domain.Member;
+import com.jigumulmi.member.dto.response.MemberDetailResponseDto;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Builder
 public class MemberListResponseDto {
 
     @Getter
-    @Builder
-    public static class MemberDto {
+    @SuperBuilder
+    public static class MemberDto extends MemberDetailResponseDto {
 
-        private LocalDateTime createdAt;
-        private Long id;
-        private String nickname;
+        private LocalDateTime modifiedAt;
         private Long KakaoUserId;
-        private LocalDateTime deregisteredAt;
 
         public static MemberDto from(Member member) {
             return MemberDto.builder()
                 .createdAt(member.getCreatedAt())
+                .modifiedAt(member.getModifiedAt())
                 .id(member.getId())
                 .nickname(member.getNickname())
+                .email(member.getEmail())
                 .KakaoUserId(member.getKakaoUserId())
                 .deregisteredAt(member.getDeregisteredAt())
                 .build();
