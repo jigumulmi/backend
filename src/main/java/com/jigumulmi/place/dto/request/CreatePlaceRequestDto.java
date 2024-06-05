348899915f1bbd9c5efd7b31a36b09ccbe0ecde9
lgkrwnsdll <prediction_HJ_KIM@predictionai.co.kr>
2024-06-06 01:10:52 +0900

    BugFix: 장소 등록 validation 정책 반영

--- src/main/java/com/jigumulmi/place/dto/request/CreatePlaceRequestDto.java
+++ src/main/java/com/jigumulmi/place/dto/request/CreatePlaceRequestDto.java
@@ -1,13 +1,11 @@
 package com.jigumulmi.place.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import java.util.List;
 
 @Getter
 @ToString
@@ -18,8 +16,6 @@ public class CreatePlaceRequestDto {
     private String name;
     @NotNull
     private Long subwayStationId;
-    @NotEmpty
     private List<String> menuList;
-    @NotBlank
     private String registrantComment;
 }
