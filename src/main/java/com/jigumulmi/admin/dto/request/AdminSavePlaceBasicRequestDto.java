874388a9524f6b0a5a98c2174fe7d2ed58b4b43f
Hakjoon Kim <lgkrwnsdll@gmail.com>
2024-08-03 02:31:01 +0900

    BugFix: 장소 기본정보 불러오기 버그 수정

--- src/main/java/com/jigumulmi/admin/dto/request/AdminSavePlaceBasicRequestDto.java
+++ src/main/java/com/jigumulmi/admin/dto/request/AdminSavePlaceBasicRequestDto.java
@@ -1,14 +1,24 @@
 package com.jigumulmi.admin.dto.request;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AdminSavePlaceBasicRequestDto {
 
-    private Long placeId;
+    @Schema(description = "장소 ID | 장소 등록 전에 기본 정보 불러오는 경우 null")
+    private Long placeId = -1L;
     private String googlePlaceId;
+
+    /**
+     * placeId에 대한 setter 메서드 오버라이딩하여 dto 생성 시 관여
+     * 명시적으로 null로 넘어오는 경우 -1L로 처리
+     * 장소 등록 전 기본 정보 불러오는 경우 장소 생성 후 정보 저장하기 위함
+     * @param placeId 장소 ID
+     */
+    public void setPlaceId(Long placeId) {
+        this.placeId = (placeId == null) ? -1L : placeId;
+    }
 }
