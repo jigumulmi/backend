48d005c37e51c88f11d2be46a04d50452c9e8048
Hakjoon Kim <lgkrwnsdll@gmail.com>
2024-11-02 10:59:25 +0900

    Feat: 리뷰 등록 (테스트 배포)

--- src/main/java/com/jigumulmi/config/WebConfig.java
+++ src/main/java/com/jigumulmi/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.jigumulmi.config;
 
 import com.jigumulmi.place.converter.PlaceCategoryGroupRequestConverter;
 import com.jigumulmi.place.converter.PlaceCategoryRequestConverter;
-import com.jigumulmi.place.domain.PlaceCategoryMapping;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
