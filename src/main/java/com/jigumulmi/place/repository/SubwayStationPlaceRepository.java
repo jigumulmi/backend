7f3f81c7c0033bcd92c1ad9aceb52d83014c88f5
Hakjoon Kim <lgkrwnsdll@gmail.com>
2024-11-29 23:29:57 +0900

    Chore: 미사용 레파지토리 삭제

--- src/main/java/com/jigumulmi/place/repository/SubwayStationPlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jigumulmi.place.repository;
-
-import com.jigumulmi.place.domain.SubwayStationPlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubwayStationPlaceRepository extends JpaRepository<SubwayStationPlace, Long> {
-
-    void deleteAllByPlaceId(Long placeId);
-}
