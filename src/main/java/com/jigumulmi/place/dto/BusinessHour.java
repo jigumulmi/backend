30205558f0b1e8d475595d66c37af7358698e42a
Hakjoon Kim <lgkrwnsdll@gmail.com>
2025-01-19 00:31:47 +0900

    Refactor: LocalTime 직렬화, 역직렬화 전역 설정

--- src/main/java/com/jigumulmi/place/dto/BusinessHour.java
+++ src/main/java/com/jigumulmi/place/dto/BusinessHour.java
@@ -1,7 +1,7 @@
 package com.jigumulmi.place.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.jigumulmi.admin.place.dto.validator.ValidBusinessHour;
+import com.jigumulmi.place.dto.time.TimeDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 import lombok.AllArgsConstructor;
@@ -27,14 +27,4 @@ public class BusinessHour {
     private LocalTime breakEnd;
     @Schema(defaultValue = "false")
     private Boolean isDayOff = false;
-
-    @JsonCreator
-    public BusinessHour(TimeDto openTime, TimeDto closeTime, TimeDto breakStart,
-        TimeDto breakEnd, Boolean isDayOff) {
-        this.openTime = TimeDto.from(openTime);
-        this.closeTime = TimeDto.from(closeTime);
-        this.breakStart = TimeDto.from(breakStart);
-        this.breakEnd = TimeDto.from(breakEnd);
-        this.isDayOff = isDayOff == null ? false : isDayOff;
-    }
 }
