841551cef712aad4fcf1e598b6523a2911e9e2b9
Hakjoon Kim <lgkrwnsdll@gmail.com>
2025-02-08 14:50:20 +0900

    Feat: 배너 개별 조회 API

--- src/main/java/com/jigumulmi/banner/BannerService.java
+++ src/main/java/com/jigumulmi/banner/BannerService.java
@@ -1,11 +1,14 @@
 package com.jigumulmi.banner;
 
+import com.jigumulmi.banner.domain.Banner;
 import com.jigumulmi.banner.dto.response.BannerPlaceListResponseDto;
 import com.jigumulmi.banner.dto.response.BannerPlaceListResponseDto.BannerPlaceDto;
 import com.jigumulmi.banner.dto.response.BannerResponseDto;
 import com.jigumulmi.banner.repository.BannerRepository;
 import com.jigumulmi.banner.repository.CustomBannerRepository;
 import com.jigumulmi.common.PagedResponseDto;
+import com.jigumulmi.config.exception.CustomException;
+import com.jigumulmi.config.exception.errorCode.CommonErrorCode;
 import com.jigumulmi.place.dto.response.SurroundingDateBusinessHour;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -30,6 +33,12 @@ public class BannerService {
             .map(BannerResponseDto::from).toList();
     }
 
+    public BannerResponseDto getBanner(Long bannerId) {
+        Banner banner = bannerRepository.findById(bannerId)
+            .orElseThrow(() -> new CustomException(CommonErrorCode.RESOURCE_NOT_FOUND));
+        return BannerResponseDto.from(banner);
+    }
+
     @Transactional(readOnly = true)
     public PagedResponseDto<BannerPlaceDto> getMappedPlaceList(Pageable pageable, Long bannerId) {
         Page<BannerPlaceDto> placePage = customBannerRepository.getAllMappedPlaceByBannerId(pageable,
@@ -47,6 +56,6 @@ public class BannerService {
         placeDtoList.forEach(
             place -> place.setCurrentOpeningStatus(surroundingBusinessHourMap.get(place.getId()), localTime));
 
-        return BannerPlaceListResponseDto.of(placeDtoList, placePage, pageable);
+        return BannerPlaceListResponseDto.of(placePage, pageable);
     }
 }
