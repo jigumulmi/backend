adf9943be7f805d68547336e22ae87d248f89298
Hakjoon Kim <lgkrwnsdll@gmail.com>
2025-02-15 22:51:09 +0900

    BugFix: RDB 저장 시 확장자 제거

--- src/main/java/com/jigumulmi/common/FileUtils.java
+++ src/main/java/com/jigumulmi/common/FileUtils.java
@@ -1,10 +1,28 @@
 package com.jigumulmi.common;
 
 import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
 
 public class FileUtils {
 
     public static String generateUniqueFilename() {
         return UUID.randomUUID().toString();
+    }
+
+    /**
+     * @param filename 경로가 포함되지 않은 파일 이름
+     * @return 확장자가 제거된 파일 이름
+     */
+    public static String getFilenameWithoutExtension(String filename) {
+        return StringUtils.substringBeforeLast(filename, ".");
+    }
+
+    /**
+     * 
+     * @param absolutePath 파일 이름이 포함된 전체 경로
+     * @return 경로가 제거된 파일 이름
+     */
+    public static String getFilenameFromAbsolutePath(String absolutePath) {
+        return StringUtils.substringAfterLast(absolutePath, "/");
     }
 }
--- src/main/java/com/jigumulmi/place/dto/MenuDto.java
+++ src/main/java/com/jigumulmi/place/dto/MenuDto.java
@@ -3,6 +3,7 @@ package com.jigumulmi.place.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.jigumulmi.aws.S3Manager;
+import com.jigumulmi.common.FileUtils;
 import com.jigumulmi.place.domain.Menu;
 import com.jigumulmi.place.domain.Place;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -25,18 +26,15 @@ public class MenuDto {
 
     private String description;
 
-    @Schema(description = "확장자 포함한 파일 이름")
+    @Schema(description = "확장자 제외한 파일 이름")
     private String fullFilename;
 
     @JsonProperty(access = Access.READ_ONLY)
     private String imageS3Key;
 
     public static MenuDto from(Menu menu) {
-        String fullFilename = null;
         String imageS3Key = menu.getImageS3Key();
-        if (imageS3Key != null) {
-            fullFilename = imageS3Key.substring(imageS3Key.lastIndexOf("/") + 1);
-        }
+        String fullFilename = FileUtils.getFilenameFromAbsolutePath(imageS3Key);
 
         return MenuDto.builder()
             .name(menu.getName())
@@ -51,8 +49,10 @@ public class MenuDto {
     public static Menu toMenu(MenuDto menuDto, Place place) {
         String imageS3Key = null;
         if (menuDto.getFullFilename() != null) {
+            String filenameWithoutExtension = FileUtils.getFilenameWithoutExtension(
+                menuDto.getFullFilename());
             imageS3Key =
-                S3Manager.MENU_IMAGE_S3_PREFIX + place.getId() + "/" + menuDto.getFullFilename();
+                S3Manager.MENU_IMAGE_S3_PREFIX + place.getId() + "/" + filenameWithoutExtension;
         }
 
         return Menu.builder()
