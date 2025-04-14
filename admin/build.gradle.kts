6189a378ef822aa89caa4e4224c256afb060a15c
hakjoon <lgkrwnsdll@naver.com>
2025-04-14 22:21:23 +0900

    Build: 빌드 재설정

---
+++ admin/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    compileOnly(project(":core"))
+
+    testImplementation(testFixtures(project(":core")))
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        freeCompilerArgs.addAll(listOf("-Xjsr305=strict"))
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
